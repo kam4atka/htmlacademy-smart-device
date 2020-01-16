6fa5ccc480927caaab70f51f7f93132853e40f28
kamcha7ka <sledsib@ya.ru>
2020-01-16 12:09:35 +0300

    9.02 - Добавит в проект результирующий код блока modal
    Добавляет код стилизации для блока modal в директорию сборки 'build'ю

--- build/js/script.js
+++ build/js/script.js
@@ -2,12 +2,73 @@
 
 (function () {
   var initialPage = function () {
-    var contactsForm = document.querySelector('.contacts__form');
-    var phoneItem = contactsForm.querySelector('#contact-phone');
-    var imPhone = new Inputmask('+7 (999) 999-99-99');
-    imPhone.mask(phoneItem);
-  };
 
+    var validatePhone = function (element) {
+      var imPhone = new Inputmask('+7 (999) 999-99-99');
+      imPhone.mask(element);
+    };
+
+    var contactsForm = document.querySelector('.contacts__form');
+    if (contactsForm) {
+      var phoneItem = contactsForm.querySelector('#contact-phone');
+      validatePhone(phoneItem);
+    }
+
+    var modalInput = document.querySelector('.header__contact-button');
+    var overlay = document.querySelector('.overlay');
+    var modal = document.querySelector('.modal');
+    if (modal) {
+      var userName = modal.querySelector('#modal-name');
+      var userPhone = modal.querySelector('#modal-phone');
+      var userMess = modal.querySelector('#modal-message');
+    }
+
+    function setCloseEvent(root) {
+      var modalClose = root.querySelector('.modal__close');
+      var clickHandler = function(evt) {
+        evt.preventDefault();
+        root.classList.add('modal_hide');
+        overlay.classList.add('overlay_hide');
+        modalClose.removeEventListener('click', clickHandler);
+        overlay.removeEventListener('click', clickHandler);
+      }
+      modalClose.addEventListener('click', clickHandler);
+      overlay.addEventListener('click', clickHandler);
+    };
+
+    function setCloseEsc(root) {
+      var escHandler = function(evt) {
+        if (evt.keyCode === 27) {
+          evt.preventDefault();
+          if (!root.classList.contains('modal_hide')) {
+            root.classList.add('modal_hide');
+          }
+          if (!overlay.classList.contains('overlay_hide')) {
+            overlay.classList.add('overlay_hide');
+          }
+          window.removeEventListener('keydown', escHandler);
+        }
+      };
+      window.addEventListener('keydown', escHandler);
+    };
+
+    function modalHandler(evt) {
+      evt.preventDefault();
+      if (overlay) {
+        overlay.classList.remove('overlay_hide');
+      }
+      if (modal) {
+        modal.classList.remove('modal_hide');
+        setCloseEvent(modal);
+        setCloseEsc(modal);
+        var modalPhoneItem = modal.querySelector('#modal-phone');
+        validatePhone(modalPhoneItem);
+      }
+    }
+
+    if (modalInput) {
+      modalInput.addEventListener('click', modalHandler);
+    }
+  };
   window.addEventListener('load', initialPage);
 })();
-
