9e26066638dcadc426484b760147c6fa8e8b8c11
kamcha7ka <sledsib@ya.ru>
2020-01-17 16:31:15 +0300

    10.01 - Добавит исправленные и дополненые javascript файлы
    Добавляет функционал скрола на страницу.

--- build/js/script.js
+++ build/js/script.js
@@ -1,12 +1,95 @@
 'use strict';
 
 (function () {
+
+  var validatePhone = function (element) {
+    var imPhone = new Inputmask('+7 (999) 999-99-99');
+    imPhone.mask(element);
+  };
+
+  function animating(object) {
+    var start = performance.now();
+    requestAnimationFrame(function animate(time) {
+      var timeFraction = (time - start) / object.duration;
+      if (timeFraction > 1) {
+        timeFraction = 1;
+      }
+      var progress = object.timing(timeFraction);
+      object.draw(progress);
+      if (timeFraction < 1) {
+        requestAnimationFrame(animate);
+      }
+    });
+  }
+
+  var scrollHandler = function (sEvt) {
+    sEvt.preventDefault();
+    var targetElement = document.querySelector(sEvt.currentTarget.href.replace(/[^#]*(.*)/, '$1'));
+    var targetY = targetElement.getBoundingClientRect().y;
+
+    var params = {
+      duration: 2000,
+      timing: function (timeFraction) {
+        return timeFraction;
+      },
+      draw: function (progress) {
+        window.scrollTo(0, progress * targetY);
+      }
+    };
+
+    animating(params);
+  };
+
   var initialPage = function () {
 
-    var validatePhone = function (element) {
-      var imPhone = new Inputmask('+7 (999) 999-99-99');
-      imPhone.mask(element);
-    };
+    function modalHandler(evt) {
+      evt.preventDefault();
+
+      var overlay = document.querySelector('.overlay');
+      var modal = document.querySelector('.modal');
+
+      function setCloseEvent(root) {
+        var modalClose = root.querySelector('.modal__close');
+        var clickHandler = function (cEvt) {
+          cEvt.preventDefault();
+          root.classList.add('modal_hide');
+          overlay.classList.add('overlay_hide');
+          modalClose.removeEventListener('click', clickHandler);
+          overlay.removeEventListener('click', clickHandler);
+        };
+        modalClose.addEventListener('click', clickHandler);
+        overlay.addEventListener('click', clickHandler);
+      }
+
+      function setCloseEsc(root) {
+        var escHandler = function (escEvt) {
+          if (escEvt.keyCode === 27) {
+            escEvt.preventDefault();
+            if (!root.classList.contains('modal_hide')) {
+              root.classList.add('modal_hide');
+            }
+            if (!overlay.classList.contains('overlay_hide')) {
+              overlay.classList.add('overlay_hide');
+            }
+            window.removeEventListener('keydown', escHandler);
+          }
+        };
+        window.addEventListener('keydown', escHandler);
+      }
+
+      if (overlay) {
+        overlay.classList.remove('overlay_hide');
+      }
+
+      if (modal) {
+        modal.classList.remove('modal_hide');
+        setCloseEvent(modal);
+        setCloseEsc(modal);
+
+        var modalPhoneItem = modal.querySelector('#modal-phone');
+        validatePhone(modalPhoneItem);
+      }
+    }
 
     var contactsForm = document.querySelector('.contacts__form');
     if (contactsForm) {
@@ -15,59 +98,13 @@
     }
 
     var modalInput = document.querySelector('.header__contact-button');
-    var overlay = document.querySelector('.overlay');
-    var modal = document.querySelector('.modal');
-    if (modal) {
-      var userName = modal.querySelector('#modal-name');
-      var userPhone = modal.querySelector('#modal-phone');
-      var userMess = modal.querySelector('#modal-message');
-    }
-
-    function setCloseEvent(root) {
-      var modalClose = root.querySelector('.modal__close');
-      var clickHandler = function(evt) {
-        evt.preventDefault();
-        root.classList.add('modal_hide');
-        overlay.classList.add('overlay_hide');
-        modalClose.removeEventListener('click', clickHandler);
-        overlay.removeEventListener('click', clickHandler);
-      }
-      modalClose.addEventListener('click', clickHandler);
-      overlay.addEventListener('click', clickHandler);
-    };
-
-    function setCloseEsc(root) {
-      var escHandler = function(evt) {
-        if (evt.keyCode === 27) {
-          evt.preventDefault();
-          if (!root.classList.contains('modal_hide')) {
-            root.classList.add('modal_hide');
-          }
-          if (!overlay.classList.contains('overlay_hide')) {
-            overlay.classList.add('overlay_hide');
-          }
-          window.removeEventListener('keydown', escHandler);
-        }
-      };
-      window.addEventListener('keydown', escHandler);
-    };
-
-    function modalHandler(evt) {
-      evt.preventDefault();
-      if (overlay) {
-        overlay.classList.remove('overlay_hide');
-      }
-      if (modal) {
-        modal.classList.remove('modal_hide');
-        setCloseEvent(modal);
-        setCloseEsc(modal);
-        var modalPhoneItem = modal.querySelector('#modal-phone');
-        validatePhone(modalPhoneItem);
-      }
-    }
-
     if (modalInput) {
       modalInput.addEventListener('click', modalHandler);
+    }
+
+    var linkList = document.querySelectorAll('.intro__link, .intro__scroll');
+    for (var i = 0; i < linkList.length; i++) {
+      linkList[i].addEventListener('click', scrollHandler);
     }
   };
   window.addEventListener('load', initialPage);
--- gulpfile.js
+++ gulpfile.js
@@ -39,6 +39,7 @@ gulp.task("server", function () {
   });
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
+  gulp.watch("source/js/*.js", gulp.series("js", "refresh"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
@@ -81,11 +82,16 @@ gulp.task("html", function () {
     .pipe(gulp.dest("build"));
 });
 
+gulp.task("js", function () {
+  return gulp.src("source/js/script.js")
+    .pipe(gulp.dest("build/js"));
+});
+
 gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
+    "source/js/inputmask.js",
     "source/img/**",
-    "source/js/**",
     "source//*.ico"
     ], {
       base: "source"
@@ -97,5 +103,5 @@ gulp.task("clean", function () {
   return del("build");
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html", "js"));
 gulp.task("start", gulp.series("build", "server"));
